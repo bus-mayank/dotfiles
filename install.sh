fae07092ed2f7d518d4c9a6a2d98a2ed35aee014
okbus/archback <mtvoid07@gmail.com>
2026-07-09 19:44:11 +0530

    Add package lists to install script

--- install.sh
+++ install.sh
@@ -13,3 +13,9 @@ mkdir -p "$HOME/.config"
 ln -sf "$DOTFILES_DIR/nvim/.config-nvim" "$HOME/.config/nvim"
 
 echo "Done. Restart your shell or run: source ~/.zshrc"
+
+echo "Installing official packages..."
+sudo pacman -S --needed - < "$DOTFILES_DIR/official-packages.txt"
+
+echo "Installing AUR packages..."
+yay -S --needed - < "$DOTFILES_DIR/aur-packages.txt"
